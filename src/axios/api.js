2206401fe2eb849998a95474d7e961d15ae26630
andribob26 <andri.feb.26@gmail.com>
2023-01-30 22:57:03 +0700

    first commit

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,7 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default axios.create({
   headers: { 'Access-Control-Allow-Origin': 'https://backend-adm-server-production.up.railway.app' },
   withCredentials: true,
   baseURL: 'https://backend-adm-server-production.up.railway.app'
 })
+
+// export default axios.create({
+//   headers: { "Access-Control-Allow-Origin": "http://localhost:2000" },
+//   withCredentials: true,
+//   baseURL: "http://localhost:2000",
+// });
