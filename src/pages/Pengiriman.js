ecc1df3893c0a7f0cbce560f2a6f68fc314e1b6c
andribob26 <andri.feb.26@gmail.com>
2023-02-22 20:43:01 +0700

    update

--- src/pages/Pengiriman.js
+++ src/pages/Pengiriman.js
@@ -332,7 +332,7 @@ const Pengiriman = () => {
             <div className="tw-relative">
               <input
                 onChange={formik.handleChange}
-                value={formik.values.noPolisi.toUpperCase()}
+                value={formik.values.noPolisi}
                 type="text"
                 className="tw-form-control tw-block tw-w-full tw-px-3 tw-py-2 tw-text-sm tw-font-normal  tw-text-gray-700 tw-bg-white tw-bg-clip-padding tw-border tw-border-solid tw-border-gray-300 tw-rounded tw-transition tw-ease-in-out tw-m-0 focus:tw-text-gray-700 focus:tw-bg-white focus:tw-border-sky-600 focus:tw-outline-none"
                 id="noPolisi"
